3d87bd2770e0fc04a12bcdb921fa43d26854bfdf
cagriefe <cagriefe34@gmail.com>
2024-07-15 14:56:52 +0300

    Try Except Examples

--- module_3.py
+++ module_3.py
@@ -569,3 +569,50 @@ y = a + 5
 a = [1, 2, 3]
 a[10]
 
+
+
+# Try Except Example
+a = 1
+
+try:
+    b = int(input("Please enter a number to divide a"))
+    a = a/b
+    print("Success a=",a)
+except:
+    print("There was an error")
+        
+
+
+
+# Try Except Specific
+# potential code before try catch
+'''
+try:
+    # code to try to execute
+except ZeroDivisionError:
+    # code to execute if there is a ZeroDivisionError
+except NameError:
+    # code to execute if there is a NameError
+except:
+    # code to execute if ther is any exception
+    
+# code that will execute if there is no exception or a one that we are handling
+'''
+
+
+
+#Try Except Specific Example
+a = 1
+
+try:
+    b = int(input("Please enter a number to divide a"))
+    a = a/b
+    print("Success a=",a)
+except ZeroDivisionError:
+    print("The number you provided cant divide 1 because it is 0")
+except ValueError:
+    print("You did not provide a number")
+except:
+    print("Something went wrong")
+    
+    
