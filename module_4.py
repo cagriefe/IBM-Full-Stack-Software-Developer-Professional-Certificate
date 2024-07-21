7dc11fd06e745c4dff87d1d2909bf8d39bd95c29
cagriefe <cagriefe34@gmail.com>
2024-07-21 14:49:13 +0300

    array addition

--- module_4.py
+++ module_4.py
@@ -567,3 +567,49 @@ max_b
 # Get the smallest value in the numpy array
 min_b = b.min()
 min_b
+
+
+## Numpy Array Operations
+
+# Array addition
+
+# Numpy array u
+u = np.array([1, 0])
+u
+
+# Numpy array v
+v = np.array([0, 1])
+v
+
+# Numpy Array Addition
+z = np.add(u, v)
+z
+
+
+# Plotting functions
+
+
+#The operation is equivalent to vector addition:
+import time 
+import sys
+import numpy as np 
+
+import matplotlib.pyplot as plt
+
+def Plotvec1(u, z, v):
+    
+    ax = plt.axes() # to generate the full window axes
+    ax.arrow(0, 0, *u, head_width=0.05, color='r', head_length=0.1)# Add an arrow to the  U Axes with arrow head width 0.05, color red and arrow head length 0.1
+    plt.text(*(u + 0.1), 'u')#Adds the text u to the Axes 
+    
+    ax.arrow(0, 0, *v, head_width=0.05, color='b', head_length=0.1)# Add an arrow to the  v Axes with arrow head width 0.05, color red and arrow head length 0.1
+    plt.text(*(v + 0.1), 'v')#Adds the text v to the Axes 
+    
+    ax.arrow(0, 0, *z, head_width=0.05, head_length=0.1)
+    plt.text(*(z + 0.1), 'z')#Adds the text z to the Axes 
+    plt.ylim(-2, 2)#set the ylim to bottom(-2), top(2)
+    plt.xlim(-2, 2)#set the xlim to left(-2), right(2)
+    
+# Plot numpy arrays
+Plotvec1(u, z, v)
+plt.show()
---
+++ test.py
@@ -0,0 +1,35 @@
+#The operation is equivalent to vector addition:
+import time 
+import sys
+import numpy as np 
+
+import matplotlib.pyplot as plt
+
+
+# Numpy array u
+u = np.array([1, 0])
+
+# Numpy array v
+v = np.array([0, 1])
+
+# Numpy Array Addition
+z = np.add(u, v)
+
+
+def Plotvec1(u, z, v):
+    
+    ax = plt.axes() # to generate the full window axes
+    ax.arrow(0, 0, *u, head_width=0.05, color='r', head_length=0.1)# Add an arrow to the  U Axes with arrow head width 0.05, color red and arrow head length 0.1
+    plt.text(*(u + 0.1), 'u')#Adds the text u to the Axes 
+    
+    ax.arrow(0, 0, *v, head_width=0.05, color='b', head_length=0.1)# Add an arrow to the  v Axes with arrow head width 0.05, color red and arrow head length 0.1
+    plt.text(*(v + 0.1), 'v')#Adds the text v to the Axes 
+    
+    ax.arrow(0, 0, *z, head_width=0.05, head_length=0.1)
+    plt.text(*(z + 0.1), 'z')#Adds the text z to the Axes 
+    plt.ylim(-2, 2)#set the ylim to bottom(-2), top(2)
+    plt.xlim(-2, 2)#set the xlim to left(-2), right(2)
+    
+# Plot numpy arrays
+Plotvec1(u, z, v)
+plt.show()
