83189bf0255406c9987b21996079b6efa7a97e40
cagriefe <cagriefe34@gmail.com>
2024-07-22 16:28:31 +0300

    API examples

--- module_5.py
+++ module_5.py
@@ -233,3 +233,76 @@ print("GET request body:",r.request.body)
 
 # We can view the form as well:
 r_post.json()['form']
+
+
+
+## Random User and Fruityvice API Examples
+
+# Objectives
+# After completing this lab you will be able to:
+
+# Load and use RandomUser API, using RandomUser() Python library
+# Load and use Fruityvice API, using requests Python library
+# Load and use Open-Joke-API, using requests Python library
+
+
+# # RandomUser API
+
+from randomuser import RandomUser
+import pandas as pd
+
+# First, we will create a random user object, r.
+r = RandomUser()
+
+
+# Then, using generate_users() function, we get a list of random 10 users.
+some_list = r.generate_users(10)
+some_list = r.generate_users(10)
+some_list
+
+# The "Get Methods" functions mentioned at the beginning of this notebook, can generate the required parameters to construct a dataset. For example, to get full name, we call get_full_name() function.
+name = r.get_full_name()
+name = r.get_full_name()
+
+# Let's say we only need 10 users with full names and their email addresses. We can write a "for-loop" to print these 10 users.
+for user in some_list:
+    print (user.get_full_name()," ",user.get_email())
+    
+
+# Generate photos of the random 10 users.
+for user in some_list:
+    print (user.get_picture())
+    
+    
+
+# To generate a table with information about the users
+def get_users():
+    users =[]
+     
+    for user in RandomUser.generate_users(10):
+        users.append({"Name":user.get_full_name(),"Gender":user.get_gender(),"City":user.get_city(),"State":user.get_state(),"Email":user.get_email(), "DOB":user.get_dob(),"Picture":user.get_picture()})
+      
+    return pd.DataFrame(users)
+
+get_users()
+df1 = pd.DataFrame(get_users())
+df1
+
+
+
+# # Fruityvice API
+import requests
+import json
+
+# We will obtain the fruityvice API data using requests.get("url") function. The data is in a json format.
+data = requests.get("https://fruityvice.com/api/fruit/all")
+
+# We will retrieve results using json.loads() function.
+results = json.loads(data.text)
+
+# We will convert our json data into pandas data frame.
+pd.DataFrame(results)
+
+# The result is in a nested json format. The 'nutrition' column contains multiple subcolumns, so the data needs to be 'flattened' or normalized.
+df2 = pd.json_normalize(results)
+df2
