f774e42e56a31c764fa153830bff5fa6365a3ee4
cagriefe <cagriefe34@gmail.com>
2024-07-23 15:20:52 +0300

    Data Analysis

--- module_5.py
+++ module_5.py
@@ -767,3 +767,33 @@ img = Image.open('./dog.jpg','r')
   
 # Output Images 
 img.show()
+
+
+
+### Data Analysis
+
+# Import pandas library
+import pandas as pd
+
+filename = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0101EN-SkillsNetwork/labs/Module%205/data/diabetes.csv"
+async def download(url, filename):
+    response = await pyfetch(url)
+    if response.status == 200:
+        with open(filename, "wb") as f:
+            f.write(await response.bytes())
+
+download(filename, "diabetes.csv")
+df = pd.read_csv("diabetes.csv")
+df
+
+
+# After reading the dataset, we can use the dataframe.head(n) method to check the top n rows of the dataframe, where n is an integer. Contrary to dataframe.head(n), dataframe.tail(n) will show you the bottom n rows of the dataframe.
+
+# show the first 5 rows using dataframe.head() method
+print("The first 5 rows of the dataframe") 
+df.head(5)
+
+# To view the dimensions of the dataframe, we use the .shape parameter.
+df.shape
+
+
