b1e96b698c5925214f7115ecb4fc70dca9b83902
cagriefe <cagriefe34@gmail.com>
2024-07-30 15:20:02 +0300

    React and Class Components

--- course-4-Developing-Front-End-Apps-with-React/module-1/src/App.jsx
+++ course-4-Developing-Front-End-Apps-with-React/module-1/src/App.jsx
@@ -4,7 +4,10 @@ function App() {
   return (
     <div>
       <h1>Hello, world!</h1>
-      <h2>It is {currDate.toLocaleDateString()} and the time now is {currDate.toLocaleTimeString()}.</h2>
+      <h2>
+        It is {currDate.toLocaleDateString()} and the time now is{" "}
+        {currDate.toLocaleTimeString()}.
+      </h2>
     </div>
   );
 }
