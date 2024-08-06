14cb1696170426a14cbf041b3e77ecb78e01783d
cagriefe <cagriefe34@gmail.com>
2024-08-06 13:32:42 +0300

    with two promises

--- course-5-Developing-Back-End-Apps-with-Node.js-and-Express/module-1/module-2/promisescript.js
+++ course-5-Developing-Back-End-Apps-with-Node.js-and-Express/module-1/module-2/promisescript.js
@@ -1,16 +1,16 @@
-//Creating a promise method. The promise will get resolved when timer times out after 6 seconds.
-let myPromise = new Promise((resolve,reject) => {
+let myPromise1 = new Promise((resolve,reject) => {
     setTimeout(() => {
-      resolve("Promise resolved")
+      resolve("Promise 1 resolved")
     },6000)})
 
-//Console log before calling the promise
-console.log("Before calling promise");
+let myPromise2 = new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve("Promise 2 resolved")
+    },3000)})
 
-//Call the promise and wait for it to be resolved and then print a message.
-myPromise.then((successMessage) => {
+  myPromise1.then((successMessage) => {
     console.log("From Callback " + successMessage)
+    myPromise2.then((successMessage) => {
+      console.log("From Callback " + successMessage)
+    })
   })
-
-//Console log after calling the promise
-  console.log("After calling promise");
