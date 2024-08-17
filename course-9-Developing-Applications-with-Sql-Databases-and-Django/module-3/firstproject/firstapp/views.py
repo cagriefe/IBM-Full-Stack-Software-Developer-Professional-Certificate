f67539fc84c94f3bfa2bd09d2b35e73c6a17e44c
cagriefe <cagriefe34@gmail.com>
2024-08-17 12:45:53 +0300

    Django app

---
+++ course-9-Developing-Applications-with-Sql-Databases-and-Django/module-3/firstproject/firstapp/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from datetime import date
+
+def index(request):
+    # Create a simple html page as a string
+    template = "<html>" \
+                "This is your first view" \
+               "</html>"
+    # Return the template as content argument in HTTP response
+    return HttpResponse(content=template)
+
+
+def get_date(request):
+    today = date.today()
+    template = "<html>" \
+                "Today's date is {}" \
+               "</html>".format(today)
+    return HttpResponse(content=template)
