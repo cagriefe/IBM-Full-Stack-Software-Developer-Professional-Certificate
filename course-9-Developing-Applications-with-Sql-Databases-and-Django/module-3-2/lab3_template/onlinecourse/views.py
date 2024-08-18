fcddc882a4e461c48c0c6fb78af1ae9d6c022f51
cagriefe <cagriefe34@gmail.com>
2024-08-18 13:24:05 +0300

    django app

---
+++ course-9-Developing-Applications-with-Sql-Databases-and-Django/module-3-2/lab3_template/onlinecourse/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import Course, Lesson, Enrollment
+from django.urls import reverse
+from django.views import generic
+from django.http import Http404
+
+# Create your views here.
+
+Course.objects.order_by('-total_enrollment')[:10]
+
+def popular_course_list(request):
+    context = {}
+    # If the request method is GET
+    if request.method == 'GET':
+        # Using the objects model manage to read all course list
+        # and sort them by total_enrollment descending
+        course_list = Course.objects.order_by('-total_enrollment')[:10]
+        # Appen the course list as an entry of context dict
+        context['course_list'] = course_list
+        return render(request, 'onlinecourse/course_list.html', context)
