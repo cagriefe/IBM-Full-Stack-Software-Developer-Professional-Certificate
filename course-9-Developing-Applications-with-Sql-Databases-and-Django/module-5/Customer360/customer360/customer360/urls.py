432fcfa20a16f0185a76b482c37a891349e137ac
cagriefe <cagriefe34@gmail.com>
2024-08-21 11:55:39 +0300

    new URL patterns and CSS

--- course-9-Developing-Applications-with-Sql-Databases-and-Django/module-5/Customer360/customer360/customer360/urls.py
+++ course-9-Developing-Applications-with-Sql-Databases-and-Django/module-5/Customer360/customer360/customer360/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index,name="index"),
+    path('create/',views.create_customer,name='create_customer'),
+    path('interact/<int:cid>',views.interact,name='interact'),
+    path('summary/',views.summary,name='summary'),
 ]
