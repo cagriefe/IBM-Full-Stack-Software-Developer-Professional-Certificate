d5b444a083c7b166b4747f6b402e3ea67fb905a6
cagriefe <cagriefe34@gmail.com>
2024-08-21 12:07:04 +0300

    new customer model and interaction channel

--- course-9-Developing-Applications-with-Sql-Databases-and-Django/module-5/Customer360/customer360/customer360/models.py
+++ course-9-Developing-Applications-with-Sql-Databases-and-Django/module-5/Customer360/customer360/customer360/models.py
@@ -6,6 +6,7 @@ class Customer(models.Model):
     email = models.EmailField(max_length=100)
     phone = models.CharField(max_length=20)
     address = models.CharField(max_length=200)
+    social_media = models.CharField(max_length=100, blank=True)
     
     def __str__(self):
         return str(self.id)
@@ -16,6 +17,7 @@ class Interaction(models.Model):
         ('phone','Phone'),
         ('sms','SMS'),
         ('email','Email'),
+        ('social_media','Social Media'),
         ('letter','Letter'),
     ]
     
--- course-9-Developing-Applications-with-Sql-Databases-and-Django/module-5/Customer360/customer360/customer360/views.py
+++ course-9-Developing-Applications-with-Sql-Databases-and-Django/module-5/Customer360/customer360/customer360/views.py
@@ -14,7 +14,8 @@ def create_customer(request):
         email = request.POST["email"]
         phone = request.POST["phone"]
         address = request.POST["address"]
-        customer = Customer.objects.create(name=name,email=email,phone=phone,address=address)
+        social_media = request.POST["social_media"]
+        customer = Customer.objects.create(name=name,email=email,phone=phone,address=address,social_media=social_media)
         customer.save()
         msg = "Successfully Saved a Customer"
         return render(request,"add.html",context={"msg":msg})
